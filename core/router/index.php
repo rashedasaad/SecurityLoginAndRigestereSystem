242fcc121e1f28c4905f2cd0fc1957b80c064e7f
Rashed Asaad <101801479+rashedasaad@users.noreply.github.com>
2022-09-14 21:43:49 +0200

    Update index.php

--- core/router/index.php
+++ core/router/index.php
@@ -5,11 +5,3 @@ $check->check_login("user","http://localhost/SecurityLoginAndRigestereSystem/cor
 echo "welcome";
 
 
-$a = ["b","2","a","1","3","s","4","g","5"];
-
-sort($a);
-
-
-for ($i=0; $i < count($a); $i++) { 
-    print $a[$i];
-}
